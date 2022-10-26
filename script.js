324b0b77e35b1b2b0050af89e92998bf27d0c496
James Murphy <jamesm8228@gmail.com>
2022-10-26 16:45:12 -0400

    Add data fetch functions and start function for displaying data

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let weather = {
-  "apiKey": "ea82df00f75875384b4f70a503f40b0a"
-}
+  apiKey: "ea82df00f75875384b4f70a503f40b0a",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  },
+  displayWeather: function(data) {
+
+  }
+};
